d9a9b641a7daf541340c96c8d2ff8cc47200708f
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-11-27 21:21:11 +0330

    adding the uuid package and adding the structure of the conversation obj and message obj

--- src/gateways/chat.gateway.ts
+++ src/gateways/chat.gateway.ts
@@ -5,11 +5,34 @@ import {
 } from '@nestjs/websockets';
 import { UseGuards } from '@nestjs/common';
 import { Server } from 'socket.io';
+import { v4 as uuid } from 'uuid';
 import { JwtSocketGuard } from 'src/guards';
 import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase';
+import { FieldValue, Filter } from '@google-cloud/firestore';
 import { Socket } from 'src/adapters';
 import { SocketPayloadType } from 'src/types';
 import { User } from 'src/entities';
+
+interface MessageObj {
+  userId: number;
+  id: string;
+  text: string;
+  createdAt: FieldValue;
+  isReaded: boolean;
+}
+
+interface ConversationDocObj {
+  id: string;
+  creatorId: number;
+  targetId: number;
+  roomId: string;
+  contributors: number[];
+  lastMessage: MessageObj | null;
+  createdAt: FirebaseFirestore.FieldValue;
+  updatedAt: FirebaseFirestore.FieldValue;
+  deletedAt: FirebaseFirestore.FieldValue | null;
+  deletedBy: number | null;
+}
 
 @WebSocketGateway({
   path: '/api/v1/user/socket/chat',
@@ -31,5 +54,5 @@ export class ChatGateWay {
   ) {}
 
   @SubscribeMessage('start-conversation')
-  startConversation(client: Socket, data: SocketPayloadType<User>) {}
+  async startConversation(client: Socket, data: SocketPayloadType<User>) {}
 }
